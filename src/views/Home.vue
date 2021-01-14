d18a071107d0c1199cec6c22d1b19c4d9e84c8c7
Mikhail Mir <mirzaakhmetov@infodev.ru>
2021-01-14 13:48:56 +1000

    Commented useless html. Create Footer-component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,307 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <section class="promo">
+    <div class="center promo__content">
+      <div class="promo__text">
+        <h1 class="promo__h1">THE BRAND</h1>
+        <h3 class="promo__h3">OF LUXERIOUS <span>FASHION</span></h3>
+      </div>
+    </div>
+  </section>
+  <section class="offers center">
+    <div class="offers_content ">
+      <div class="offer_right">
+        <div class="men_text">
+          <div class="text">
+            <h3 class="offers__h3">HOT DEAL</h3>
+            <h1 class="offers__h1">FOR MEN</h1>
+          </div>
+        </div>
+        <div class="accesories_text">
+          <div class="text">
+            <h3 class="offers__h3">LUXIROUS & TRENDY</h3>
+            <h1 class="offers__h1">ACCESORIES</h1>
+          </div>
+        </div>
+      </div>
+      <div class="offer_left">
+        <div class="women_text">
+          <div class="text">
+            <h3 class="offers__h3">30% OFFER</h3>
+            <h1 class="offers__h1">WOMEN</h1>
+          </div>
+        </div>
+        <div class="kids_text">
+          <div class="text">
+            <h3 class="offers__h3">NEW ARRIVALS</h3>
+            <h1 class="offers__h1">FOR KIDS</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="catalog center">
+    <h1 class="catalog__h1">Fetured Items</h1>
+    <p class="catalog__p">
+      Shop for items based on what we fetured in this week
+    </p>
+  </section>
+
+  <section class="center product-box">
+    <div class="product">
+      <a href="shoping_card.html"
+        ><img
+          class="product__img"
+          src="@/assets/img/product/product-1.png"
+          alt="product 1"
+      /></a>
+
+      <div class="product__content">
+        <a href="#" class="product__name">Mango People T-shirt</a>
+        <div class="product__price">$52.00</div>
+      </div>
+      <a class="product__add" href="#">Add to Cart</a>
+    </div>
+    <div class="product">
+      <a href="#"
+        ><img
+          class="product__img"
+          src="@/assets/img/product/product-2.png"
+          alt="product 2"
+      /></a>
+      <div class="product__content">
+        <a href="#" class="product__name">Mango People T-shirt</a>
+        <div class="product__price">$52.00</div>
+      </div>
+      <a class="product__add" href="#">Add to Cart</a>
+    </div>
+    <div class="product">
+      <a href="#"
+        ><img
+          class="product__img"
+          src="@/assets/img/product/product-3.png"
+          alt="product 3"
+      /></a>
+      <div class="product__content">
+        <a href="#" class="product__name">Mango People T-shirt</a>
+        <div class="product__price">$52.00</div>
+      </div>
+      <a class="product__add" href="#">Add to Cart</a>
+    </div>
+    <div class="product">
+      <a href="#"
+        ><img
+          class="product__img"
+          src="@/assets/img/product/product-4.png"
+          alt="product 4"
+      /></a>
+      <div class="product__content">
+        <a href="#" class="product__name">Mango People T-shirt</a>
+        <div class="product__price">$52.00</div>
+      </div>
+      <a class="product__add" href="#">Add to Cart</a>
+    </div>
+    <div class="product">
+      <a href="#"
+        ><img
+          class="product__img"
+          src="@/assets/img/product/product-5.png"
+          alt="product 5"
+      /></a>
+      <div class="product__content">
+        <a href="#" class="product__name">Mango People T-shirt</a>
+        <div class="product__price">$52.00</div>
+      </div>
+      <a class="product__add" href="#">Add to Cart</a>
+    </div>
+    <div class="product">
+      <a href="#"
+        ><img
+          class="product__img"
+          src="@/assets/img/product/product-6.png"
+          alt="product 6"
+      /></a>
+      <div class="product__content">
+        <a href="#" class="product__name">Mango People T-shirt</a>
+        <div class="product__price">$52.00</div>
+      </div>
+      <a class="product__add" href="#">Add to Cart</a>
+    </div>
+    <div class="product">
+      <a href="#"
+        ><img
+          class="product__img"
+          src="@/assets/img/product/product-7.png"
+          alt="product 7"
+      /></a>
+      <div class="product__content">
+        <a href="#" class="product__name">Mango People T-shirt</a>
+        <div class="product__price">$52.00</div>
+      </div>
+      <a class="product__add" href="#">Add to Cart</a>
+    </div>
+    <div class="product">
+      <a href="#"
+        ><img
+          class="product__img"
+          src="@/assets/img/product/product-8.png"
+          alt="product 8"
+      /></a>
+      <div class="product__content">
+        <a href="#" class="product__name">Mango People T-shirt</a>
+        <div class="product__price">$52.00</div>
+      </div>
+      <a class="product__add" href="#">Add to Cart</a>
+    </div>
+    <div><a class="button" href="#">Browse All Products</a></div>
+  </section>
+
+  <section class="center offer_main">
+    <div class="offer_main_foto">
+      <div class="offer_text">
+        <h1>30% <span>OFFER </span></h1>
+        <h3>FOR WOMEN</h3>
+      </div>
+    </div>
+    <div class="options">
+      <div class="option_content">
+        <div>
+          <a href="#"><img src="@/assets/img/car.png"/></a>
+        </div>
+        <div class="option_text">
+          <h3>Free Delivery</h3>
+          <p>
+            Worldwide delivery on all. Authorit tively morph next-generation
+            innovation with extensive models.
+          </p>
+        </div>
+      </div>
+      <div class="option_content">
+        <div>
+          <a href="#"><img src="@/assets/img/1340.png"/></a>
+        </div>
+        <div class="option_text">
+          <h3>Sales & discounts</h3>
+          <p>
+            Worldwide delivery on all. Authorit tively morph next-generation
+            innovation with extensive models.
+          </p>
+        </div>
+      </div>
+      <div class="option_content">
+        <div>
+          <a href="#"><img src="@/assets/img/1341.png"/></a>
+        </div>
+        <div class="option_text">
+          <h3>Sales & discountse</h3>
+          <p>
+            Worldwide delivery on all. Authorit tively morph next-generation
+            innovation with extensive models
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="subscribe center">
+    <article class="subscribe_content">
+      <div class="subscribe_content_left">
+        <div><img src="@/assets/img/face.png" /></div>
+        <div class="subscribe_content_left_text">
+          <p class="subscribe__p">
+            “Vestibulum quis porttitor dui! Quisque viverra nunc mi, a pulvinar
+            purus condimentum a. Aliquam condimentum mattis neque sed pretium”
+            <br />
+            <span class="subscribe__h4">Bin Burhan</span> <br />
+            <span class="subscribe__p1">Dhaka, Bd</span>
+          </p>
+        </div>
+      </div>
+      <div class="subscribe_content_right">
+        <div class="subscribe_content_right_text">
+          <h2 class="subscribe__h2">Subscribe</h2>
+          <h1 class="subscribe__h1">FOR OUR NEWLETTER AND PROMOTION</h1>
+        </div>
+        <form class="subscribe_form">
+          <input type="email" placeholder="Enter Your Email..." required />
+          <button>Subscribe</button>
+        </form>
+      </div>
+    </article>
+    <div class="pagination">
+      <a class="lines" href="#"></a>
+      <a class="lines active" href="#"></a>
+      <a class="lines" href="#"></a>
+    </div>
+  </section>
+
+  <section class="footer center">
+    <article class="contacts">
+      <div class="about_brand">
+        <a href="" class="logo"
+          ><img
+            class="logo__img"
+            src="@/assets/img/logo.png"
+            alt="logo"
+          />BRAN<span>D</span></a
+        >
+        <p>
+          Objectively transition extensive data rather than cross functional
+          solutions. Monotonectally syndicate multidisciplinary materials before
+          go forward benefits. Intrinsicly syndicate an expanded array of
+          processes and cross-unit partnerships.
+        </p>
+        <p>
+          Efficiently plagiarize 24/365 action items and focused infomediaries.
+          Distinctively seize superior initiatives for wireless technologies.
+          Dynamically optimize.
+        </p>
+        <p>
+          Objectively strategize seamless relationships via resource sucking
+          testing procedures. Proactively cultivate next-generation results for
+          value-added methodologies. Dynamically plagiarize unique methodologies
+          after performance based methodologies.
+        </p>
+        <p>
+          Monotonectally empower stand-alone mindshare rather than ubiquitous
+          opportunities. Dynamically orchestrate resource sucking scenarios
+          after alternative synergy.
+        </p>
+        <p>
+          Compellingly envisioneer corporate methods of empowerment before
+          standards compliant technologies. Objectively facilitate progressive.
+        </p>
+      </div>
+      <nav class="company">
+        <ul>
+          <li><h2 class="footer_h2">COMPANY</h2></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Shope</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">How it works</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+      <nav class="information">
+        <ul>
+          <li><h2 class="footer_h2">INFORMATION</h2></li>
+          <li><a href="#">Tearms & Condition</a></li>
+          <li><a href="#">Privacy Policy</a></li>
+          <li><a href="#">How to Buy</a></li>
+          <li><a href="#">How to Sell</a></li>
+          <li><a href="#"> Promotion</a></li>
+        </ul>
+      </nav>
+      <nav class="category">
+        <ul>
+          <li><h2 class="footer_h2">SHOP CATEGORY</h2></li>
+          <li><a href="#">Men</a></li>
+          <li><a href="#">Women</a></li>
+          <li><a href="#">Child</a></li>
+          <li><a href="#">Apparel</a></li>
+          <li><a href="#">Brows All</a></li>
+        </ul>
+      </nav>
+    </article>
+  </section>
 </template>
 
 <script lang="ts">

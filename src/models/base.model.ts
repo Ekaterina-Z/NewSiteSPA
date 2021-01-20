b6c01e528b42ca32aaac34f71f9ecc600d147f2c
Ekaterina-Z <motozorlik@gmail.com>
2021-01-20 12:34:19 +0300

    добавлен файл product.json

--- src/models/base.model.ts
+++ src/models/base.model.ts
@@ -7,10 +7,10 @@ export class UserCart {
   totalCost: number;
   products: Product[];
   constructor(products: Product[]) {
-    this.products = products
+    this.products = products;
     this.totalCost = this.products.reduce(
       (accumulator: number, product: Product) => accumulator + product.amount,
       0
     );
-  }
+  } //* это функция расчета общей суммы в корзине ? в массив Product[] откуда будут данные попадать ?
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,24 +1,24 @@
 import { createStore } from "vuex";
 import { UserCart } from "@/models/base.model";
 
-const state = {cart: UserCart}
-type stateType = typeof state;
+const state = { cart: UserCart }; //* - Это мы привязываемся к состоянию?
+type stateType = typeof state; //* - зачем нам типизация ?
 
 const getters = {
   cart: (state: stateType) => state.cart
-}
+};
 
 export default createStore({
   state,
   getters,
   mutations: {
     setCart: (state: stateType, payload: typeof UserCart) => {
-      state.cart = payload;
+      state.cart = payload; //*  Здесь мы отдаем данные в корзину?
     }
   },
   actions: {
     setCart: (context, payload: typeof UserCart) => {
-      context.commit("setCart", payload);
+      context.commit("setCart", payload);//*  Здесь мы получаем данные из корзины после обновления state?
     }
   },
   modules: {}
